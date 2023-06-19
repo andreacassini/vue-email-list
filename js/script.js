13b6f5f6524b8811615aa82a9d64d76051afe302
andreacassini <andrea1316@hotmail.it>
2023-06-19 15:26:53 +0200

    es done

--- js/script.js
+++ js/script.js
@@ -3,10 +3,14 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-
+            emails: []
         }
     },
-    mounted: [
-
-    ]
+    mounted() {
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
+                this.emails.push(response.data.response);
+            })
+        }
+    }
 }).mount('#app')
